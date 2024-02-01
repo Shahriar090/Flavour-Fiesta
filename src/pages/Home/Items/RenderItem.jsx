b51b2c893f269bd277f1d9128aada3b4363efd05
Shahriar <shahriarh655@gmail.com>
2024-02-01 21:19:12 +0600

    Making components cleaner by using tailwind class component

--- src/pages/Home/Items/RenderItem.jsx
+++ src/pages/Home/Items/RenderItem.jsx
@@ -3,21 +3,13 @@ import React from "react";
 const RenderItem = ({ singleItem }) => {
   const { item, itemImg, itemDes } = singleItem;
   return (
-    <div className="card-container flex flex-col gap-6 shadow-lg p-4 rounded-md">
+    <div className="card-container ">
       <picture>
-        <img
-          src={itemImg}
-          alt=""
-          className="w-full h-auto lg:h-[250px] rounded-md"
-        />
+        <img src={itemImg} alt="" className="card-img" />
       </picture>
       <div>
-        <h1 className="text-xl lg:text-2xl font-bold text-black font-mono">
-          {item}
-        </h1>
-        <p className="text-lg font-sans text-gray-700 max-w-[40ch]">
-          {itemDes}
-        </p>
+        <h1 className="card-title">{item}</h1>
+        <p className="card-des">{itemDes}</p>
       </div>
     </div>
   );
